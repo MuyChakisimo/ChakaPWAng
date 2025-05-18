e15d8592d7d4ef81cc7028a0059a5349994dbc3a
MuyChakisimo <speakervibe@gmail.com>
2025-05-18 08:15:34 -0700

    Update service-worker.js
    Auto Updates

--- service-worker.js
+++ service-worker.js
@@ -1,19 +1,42 @@
-self.addEventListener('install', function (e) {
-    e.waitUntil(
-        caches.open('chaka-cache').then(function (cache) {
-            return cache.addAll([
-                '/index.html',
-                '/manifest.json',
-                '/icon.png'
-            ]);
-        })
-    );
+const CACHE_NAME = 'chaka-cache-v1';
+const FILES_TO_CACHE = [
+  '/ChakaPWAng/',
+  '/ChakaPWAng/index.html',
+  '/ChakaPWAng/manifest.json',
+  '/ChakaPWAng/icon.png',
+  '/ChakaPWAng/changelog.json',
+];
+
+self.addEventListener('install', event => {
+  console.log('[ServiceWorker] Install');
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+  self.skipWaiting();
 });
 
-self.addEventListener('fetch', function (e) {
-    e.respondWith(
-        caches.match(e.request).then(function (response) {
-            return response || fetch(e.request);
-        })
-    );
+self.addEventListener('activate', event => {
+  console.log('[ServiceWorker] Activate');
+  event.waitUntil(
+    caches.keys().then(keyList => {
+      return Promise.all(keyList.map(key => {
+        if (key !== CACHE_NAME) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
 });
