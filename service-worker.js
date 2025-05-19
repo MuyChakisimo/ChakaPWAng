1d4c10903d1baa2eb87134aac4edd0cd8e7fb574
MuyChakisimo <speakervibe@gmail.com>
2025-05-18 17:03:09 -0700

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
   '/ChakaPWAng/manifest.json',
   '/ChakaPWAng/icon.png',
   '/ChakaPWAng/changelog.json',
-  '/ChakaPWAng/monkey.GIF',
+  '/ChakaPWAng/monkey.gif',
 ];
 
 self.addEventListener('install', event => {
